5f269e144bda6c963de254ae797238c27537c48e
Oleksandr Leonov <oleksandr.leonov.work@gmail.com>
2024-07-15 11:47:18 +0300

    Basic configuration.

---
+++ webpack.renderer.config.ts
@@ -0,0 +1,27 @@
+import type { Configuration } from 'webpack';
+
+import { rules } from './webpack.rules';
+import { plugins } from './webpack.plugins';
+import * as path from "path";
+
+rules.push({
+  test: /\.css$/,
+  use: [
+    { loader: 'style-loader' },
+    { loader: 'css-loader' },
+    { loader: 'postcss-loader' }
+  ]
+})
+
+export const rendererConfig: Configuration = {
+  module: {
+    rules,
+  },
+  plugins,
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+    alias: {
+      '@': path.resolve(__dirname, 'src/renderer')
+    }
+  },
+};
